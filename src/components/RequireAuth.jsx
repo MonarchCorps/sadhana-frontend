f6eafcabb0e125497ad8fb404dd6628d41bc139a
Monarch Corps <monarchcorps01@gmail.com>
2024-11-23 22:27:04 +0100

    Added strict access so that each user can only visit a specific page based on their roles

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -2,17 +2,47 @@
 import { useLocation, Navigate, Outlet } from 'react-router-dom'
 import useAuth from '../hooks/useAuth'
 
+const ROLES = {
+    User: parseInt(import.meta.env.VITE_USER_CODE),
+    Instructor: parseInt(import.meta.env.VITE_INSTRUCTOR_CODE),
+    Admin: parseInt(import.meta.env.VITE_ADMIN_CODE)
+}
+
 const RequireAuth = ({ allowedRoles }) => {
     const { auth } = useAuth();
     const location = useLocation();
 
-    return (
-        auth?.roles?.find(role => allowedRoles?.includes(role))
-            ? <Outlet />
-            : auth?.username
-                ? <Navigate to='/unauthorized' state={{ from: location }} replace />
-                : <Navigate to='/auth' state={{ from: location }} replace />
-    )
+    if (!auth?.roles || auth.roles.length === 0) {
+        return <Navigate to="/auth" state={{ from: location }} replace />;
+    }
+
+    const hasAdminRole = auth?.roles?.includes(ROLES.Admin);
+    const hasInstructorRole = auth?.roles?.includes(ROLES.Instructor);
+    const hasUserRole = auth?.roles?.includes(ROLES.User);
+
+    if (allowedRoles.includes(ROLES.Admin)) {
+        if (hasUserRole && hasInstructorRole && hasAdminRole) {
+            return <Outlet />
+        } else {
+            return <Navigate to='/unauthorized' state={{ from: location }} replace />
+        }
+    }
+
+    if (allowedRoles.includes(ROLES.Instructor)) {
+        if (hasUserRole && hasInstructorRole && !hasAdminRole) {
+            return <Outlet />
+        } else {
+            return <Navigate to='/unauthorized' state={{ from: location }} replace />
+        }
+    }
+
+    if (allowedRoles.includes(ROLES.User)) {
+        if (hasUserRole && !hasInstructorRole && !hasAdminRole) {
+            return <Outlet />
+        } else {
+            return <Navigate to='/unauthorized' state={{ from: location }} replace />
+        }
+    }
 
 }
 
