eda97619806f4d13dea9a56428debbf6f5f57be1
Monarch Corps <monarchcorps01@gmail.com>
2024-11-24 23:45:43 +0100

    Changed loader ui

--- src/components/Loaders/Loading.jsx
+++ src/components/Loaders/Loading.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable react/prop-types */
+import botLogo from '../../assets/images/9c7d37cd-ba05-4987-8a75-c5376ac4990d.png'
+
 function Loading({ isLoading, text = '', bgColor }) {
     return (
         isLoading && (
             <div className={`fixed top-0 left-0 w-full h-full overflow-hidden z-[1000] grid place-items-center ${bgColor ? `bg-[${bgColor}]` : 'bg-[#ffffff80]'}`}>
-                <div className='spinner'>
+                <img src={botLogo} />
+                {/* <div className='spinner'>
                     <div></div>
                     <div></div>
                     <div></div>
@@ -19,7 +22,7 @@ function Loading({ isLoading, text = '', bgColor }) {
                     text && (
                         <p className='mt-24'>{text}</p>
                     )
-                }
+                } */}
             </div>
         )
     )
