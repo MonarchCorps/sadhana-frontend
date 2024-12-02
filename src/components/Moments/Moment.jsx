0815a66e6e94069ca86d211ef9526c70d972da67
Monarch Corps <monarchcorps01@gmail.com>
2024-12-02 01:13:49 +0100

    Moment section responsive

--- src/components/Moments/Moment.jsx
+++ src/components/Moments/Moment.jsx
@@ -4,8 +4,8 @@ import RightMoment from "./RightMoment"
 function Moment() {
     return (
         <section className="bg-[url('./assets/images/close-up-of-a-man-in-white-sportswear-doing-yoga-in-a-fitness-room-with-a-balgovon-the-concept-of-a.jpg')] bg-no-repeat bg-center w-full bg-cover mt-28">
-            <div className="py-[5.2rem] px-10 bg-[#1a1a1a63] h-full">
-                <div className="max-w-[94%] mx-auto flex gap-4">
+            <div className="py-[3rem] px-10 hrmd:px-5 bg-[#1a1a1a63] h-full">
+                <div className="flex hrmd:flex-col gap-4">
                     <LeftMoment />
                     <RightMoment />
                 </div>
--- src/components/Moments/RightMoment.jsx
+++ src/components/Moments/RightMoment.jsx
@@ -2,8 +2,8 @@ import { FaLeaf } from "react-icons/fa"
 
 function RightMoment() {
     return (
-        <div className="w-[54%]">
-            <div className="border-2 border-dashed border-[#eeeeee28] p-14 rounded-md overflow-hidden text-center">
+        <div className="w-[64%] hrmd:w-full">
+            <div className="border-2 border-dashed border-[#eeeeee28] p-14 hrmd:px-8 rounded-md overflow-hidden text-center">
                 <div className="mb-5 max-w-[40%] mx-auto flex items-center gap-3">
                     <div className="bg-[#E7CBAE] h-[0.095rem] w-full"></div>
                     <span className="text-[#E7CBAE] text-xl">
@@ -11,11 +11,11 @@ function RightMoment() {
                     </span>
                     <div className="bg-[#E7CBAE] h-[0.095rem] w-full"></div>
                 </div>
-                <h1 className="text-[#f3f1f1] text-4xl font-600 leading-[1.32] mb-5">
+                <h1 className="text-[#f3f1f1] text-3xl font-600 leading-[1.32] mb-5 hrmd:text-2xl md:text-xl">
                     Each Moment Is An Opportunity To Be Kind To Yourself And Your Body
                 </h1>
-                <p className="text-[#f3f1f1] text-base leading-[1.6]">
-                    Per sociosqu ultrices ante egestas elementum odio dignissim donec sed. Luctus tellus euismod netus felis phasellus mollis fusce. Condimentum diam parturient feugiat vitae consectetuer sit tincidunt sem neque aptent tellus.
+                <p className="text-[#f3f1f1] text-base leading-[1.5] hrmd:text-sm">
+                    Per sociosqu ultrices ante egestas elementum odio dignissim donec sed. Luctus tellus euismod netus felis phasellus mollis fusce. Condimentum diam parturient feugiat vitae consectetuer.
                 </p>
             </div>
         </div>
