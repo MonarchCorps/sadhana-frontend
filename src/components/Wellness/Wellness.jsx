1b191e102c34a76ce9035a070478f6094e8b048c
Monarch Corps <monarchcorps01@gmail.com>
2024-12-02 17:43:50 +0100

    Wellness section responsive

--- src/components/Wellness/Wellness.jsx
+++ src/components/Wellness/Wellness.jsx
@@ -1,16 +1,16 @@
 function Wellness() {
     return (
-        <section className='pt-28'>
-            <div className='bg-[url("./assets/images/glad-calm-slim-millennial-european-female-in-sportswear-practicing-yoga-enjoy-training-meditating.jpg")] bg-no-repeat size-full -z-10' style={{ backgroundSize: '100%', backgroundPosition: 'center bottom -8rem' }}>
-                <div className="py-[4.2rem] px-10 bg-[#1a1a1a63] h-full">
-                    <div className="max-w-[50%] p-4 h-full">
-                        <h1 className="text-[2.8rem] leading-[1.18] font-600 font-sans mb-6 text-[#eee9e9]">
+        <section className="bg-[url('./assets/images/glad-calm-slim-millennial-european-female-in-sportswear-practicing-yoga-enjoy-training-meditating.jpg')] bg-no-repeat bg-center w-full bg-cover mt-28">
+            <div className="py-[3rem] px-10 hrmd:px-0 bg-[#1a1a1a63]">
+                <div className="py-5s px-10 h-full">
+                    <div className="max-w-[50%] p-4 sm:p-2 h-full lg:max-w-[100%]">
+                        <h1 className="text-[2.5rem] leading-[1.18] font-600 font-sans mb-6 text-[#eee9e9] lg:text-3xl sm:text-2xl">
                             Let the threads of life get connected with yoga
                         </h1>
-                        <p className="text-[15px] leading-[1.69] font-400 opacity-80 mb-6 text-[#eee9e9]">
+                        <p className="text-[15px] leading-[1.69] font-400 opacity-80 mb-6 text-[#eee9e9] hrmd:text-sm sm:text-xs">
                             At posuere potenti montes ligula risus. Metus suspendisse ornare senectus taciti molestie dis arcu duis. Fusce mus sit eget nibh curae id montes finibus venenatis hendrerit.
                         </p>
-                        <button className='text-sm h-14 w-36 mr-2 bg-[#e5759a] rounded-full text-slate-50 shadow-inner p-1 mt-6'>Discover more</button>
+                        <button className='text-sm h-14 w-36 hrmd:h-12 hrmd:w-32 hrmd:text-xs mr-2 bg-[#e5759a] rounded-full text-slate-50 shadow-inner p-1 mt-6'>Discover more</button>
                     </div>
                 </div>
             </div>
