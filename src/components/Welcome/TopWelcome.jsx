93e6d6d1dd67b9cec09a5d295fd542609cc88b15
Monarch Corps <monarchcorps01@gmail.com>
2024-12-19 22:52:00 +0100

    Removed dummy text

--- src/components/Welcome/TopWelcome.jsx
+++ src/components/Welcome/TopWelcome.jsx
@@ -26,12 +26,12 @@ function TopWelcome() {
             </div>
             <div className="w-[36%] hrmd:w-full">
                 <p className="text-[15px] leading-[1.69] text-[#3a3939] font-400 opacity-80 sm:text-sm">
-                    Aliquet diam mattis phasellus fames cras maximus. Semper interdum pulvinar habitasse ligula hendrerit. Tellus vestibulum elit senectus donec fusce feugiat rhoncus. Integer arcu tempor posuere leo accumsan tristique hendrerit orci inceptos.
+                    A quiet mind is essential for mental clarity and emotional stability. It helps reduce stress, improve focus, and enhance overall well-being, creating space for peace, creativity, and healthier living.
                 </p>
             </div>
             <div className="w-[36%] hrmd:w-full">
                 <p className="text-[15px] leading-[1.69] text-[#3a3939] font-400 opacity-80 sm:text-sm">
-                    Senectus penatibus himenaeos pharetra massa rutrum ornare nascetur. Nascetur leo suspendisse habitant vulputate nisl convallis praesent maecenas himenaeos. Pharetra ipsum vivamus fames nostra accumsan diam lacus.
+                    In a world full of distractions, a quiet mind offers the clarity needed for personal growth. It calms the body, reduces anxiety, and nurtures emotional resilience, fostering a healthier, balanced life.
                 </p>
             </div>
         </div>
